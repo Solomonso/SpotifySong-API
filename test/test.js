fca79d0653511c5e76f0cf0c8b648c061cb36c58
solomon <Solomonn135@gmail.com>
2022-04-01 14:22:45 +0200

    working on the integration test for a sing song

--- test/test.js
+++ test/test.js
@@ -1,4 +1,28 @@
+import {ApolloServer, gql} from "apollo-server-express";
+import {makeExecutableSchema} from '@graphql-tools/schema';
+import typeDefs from "../src/schema/typedefs";
+import resolvers from "../src/schema/resolvers";
+const schema =  makeExecutableSchema({
+    typeDefs,
+    resolvers,
+});
 
+const server = async () => { new ApolloServer({
+    schema
+});
 
-await server.executeOperation({query: QUERY});
-await server.executeOperation({query: MUTATION});
+}
+test("Get a single song", async () => {
+    const GET_A_SINGLE_SONG = gql`
+    query {
+        getSong(id: "53209e68-b2c9-404b-8037-5c458be9fc7c") {
+            artistName
+        }
+    }`;
+    const {
+        data: {getSong}
+    } = await server.executeOperation({query: GET_A_SINGLE_SONG});
+    expect(getSong).toEqual({name: "Tones and I"})
+});
+// await server.executeOperation({query: QUERY});
+// await server.executeOperation({query: MUTATION});
