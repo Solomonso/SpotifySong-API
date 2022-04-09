c015b1701c78d8815ed828d14984000f0de73672
solomon <Solomonn135@gmail.com>
2022-04-09 22:12:50 +0200

    Added integration test for the song query and mutation

---
+++ test/resolver.js
@@ -0,0 +1,90 @@
+const songs = [
+    {
+        id: "53209e68-b2c9-404b-8037-5c458be9fc7c",
+        trackName: "Dance Monkey",
+        artistName: "Tones and I",
+        genre: "australian pop",
+        danceability: 82,
+        valence: 54,
+        popularity: 83,
+
+    },
+    {
+        id: "bfb2f3d6-cfcd-43c4-961e-928cc290574c",
+        trackName: "Beautiful People (feat. Khalid)",
+        artistName: "Shawn Mendes",
+        genre: "pop",
+        danceability: 64,
+        valence: 55,
+        popularity: 86
+    },
+    {
+        id: "df3741ff-94c9-4587-a029-cf9f32e954bf",
+        trackName: "Goodbyes (Feat. Young Thug)",
+        artistName: "Post Malone",
+        genre: "dfw rap",
+        danceability: 58,
+        valence: 18,
+        popularity: 94
+    },
+    {
+        id: "cf240631-9b1c-42b0-a843-ee0799dd2dc2",
+        trackName: "I Don't Care (with Justin Bieber)",
+        artistName: "Ed Sheeran",
+        genre: "pop",
+        danceability: 80,
+        valence: 84,
+        popularity: 84
+    }
+]
+
+export const getAllSongs = () => {
+    return songs;
+}
+export default {
+    Query: {
+        getSong: (parent, {id}) => {
+        const song = songs.find(song => song.id === id);
+            if(song) {
+                return song;
+            } else {
+                throw new Error("Song not found!");
+            }
+        },
+        allSongs() {
+            return songs;
+        },
+    },
+
+    Mutation: {
+        createSong: (parent, args) => {
+            const newSong = args;
+            newSong.push(newSong);
+            return newSong
+        },
+        deleteSong: (parent, {id}) => {
+            const index = songs.findIndex(song => song.id === id);
+            if(index >= 0) {
+                songs.splice(index,1);
+                return "Deleted Successfully!";
+            } else {
+                throw new Error("Delete failed!");
+            }
+        },
+        updateSong:  (parent, args) => {
+            const song = songs.find(song => song.id === args.id);
+            if(song) {
+                song.trackName = args.trackName;
+                song.artistName = args.artistName;
+                song.genre = args.genre;
+                song.danceability = args.danceability;
+                song.valence = args.valence;
+                song.popularity = args.popularity;
+                return "Updated Successfully!";
+            } else {
+                throw new Error("Updated Failed!");
+            }
+        }
+
+    }
+};
--- test/test.js
+++ test/test.js
@@ -1,28 +1,141 @@
 import {ApolloServer, gql} from "apollo-server-express";
-import {makeExecutableSchema} from '@graphql-tools/schema';
-import typeDefs from "../src/schema/typedefs";
-import resolvers from "../src/schema/resolvers";
-const schema =  makeExecutableSchema({
+import typeDefs from "./schema";
+import resolvers from "./resolver";
+import {getAllSongs} from "./resolver";
+
+const server = new ApolloServer({
     typeDefs,
-    resolvers,
+    resolvers
 });
+test('A single song', async () => {
 
-const server = async () => { new ApolloServer({
-    schema
-});
-
-}
-test("Get a single song", async () => {
     const GET_A_SINGLE_SONG = gql`
-    query {
-        getSong(id: "53209e68-b2c9-404b-8037-5c458be9fc7c") {
+     query GetSong($getSongId: ID!) {
+        getSong(id: $getSongId) {
             artistName
         }
     }`;
-    const {
-        data: {getSong}
-    } = await server.executeOperation({query: GET_A_SINGLE_SONG});
-    expect(getSong).toEqual({name: "Tones and I"})
+
+    const { data: {getSong} } = await server.executeOperation({
+        query: GET_A_SINGLE_SONG,
+        variables: {getSongId: "53209e68-b2c9-404b-8037-5c458be9fc7c"}
+    });
+    expect(getSong).toEqual({ artistName: "Tones and I" });
 });
-// await server.executeOperation({query: QUERY});
-// await server.executeOperation({query: MUTATION});
+
+test('Song not found', async () => {
+    const GET_A_SINGLE_SONG = gql`
+     query GetSong($getSongId: ID!) {
+        getSong(id: $getSongId) {
+            artistName
+        }
+    }`;
+
+    const { errors: [error] } = await server.executeOperation({
+        query: GET_A_SINGLE_SONG,
+        variables: {getSongId: "53209e68-b2c9-404b-8037-5c458be9fcs"}
+    });
+    expect(error.message).toEqual("Song not found!");
+})
+test('All songs', async () => {
+    const GET_ALL_SONGS = gql `
+     query AllSongs {
+          allSongs {
+            id
+            artistName
+            danceability
+            genre
+            trackName
+            valence
+            popularity
+          }
+        }`;
+
+    const {data: {allSongs} } = await server.executeOperation({
+        query: GET_ALL_SONGS
+    });
+    expect(allSongs).toEqual(getAllSongs());
+});
+
+test('Add song', async () => {
+    const ADD_SONG = gql`
+    mutation CreateSong($id: ID!, $trackName: String!, $artistName: String!, $genre: String!, $danceability: Int, $valence: Int, $popularity: Int) {
+          createSon(id: $id, trackName: $trackName, artistName: $artistName, genre: $genre, danceability: $danceability, valence: $valence, popularity: $popularity) {
+            artistName
+            id
+          }
+        }`;
+
+    const {data: {createSong} } = await server.executeOperation({
+        query: ADD_SONG,
+    });
+    expect(createSong).toEqual({
+        "id": "ff0eb786-969c-4125-b2d9-0f61e788202c",
+        "trackName": "Alot",
+        "artistName": "21 Savage ft J cole",
+        "genre": "Rap",
+        "danceability": 65,
+        "valence": 78,
+        "popularity": 99
+    });
+});
+
+test('Delete song', async () => {
+    const DELETE_SONG = gql`
+        mutation DeleteSong($deleteSongId: ID!) {
+          deleteSong(id: $deleteSongId) 
+        }`;
+    const {data: {deleteSong} } = await server.executeOperation({
+        query: DELETE_SONG,
+        variables: {deleteSongId: "53209e68-b2c9-404b-8037-5c458be9fc7c"}
+    });
+    expect(deleteSong).toEqual("Deleted Successfully!");
+});
+
+test('Delete song failed', async () => {
+    const DELETE_SONG = gql`
+        mutation DeleteSong($deleteSongId: ID!) {
+          deleteSong(id: $deleteSongId) 
+        }`;
+    const {errors: [error] } = await server.executeOperation({
+        query: DELETE_SONG,
+        variables: {deleteSongId: "63209788-b2c9-404b-8037-5c458be9fc7er"}
+    });
+    expect(error.message).toEqual("Delete failed!");
+});
+
+test('Update song', async () => {
+    const UPDATE_SONG = gql`
+        mutation UpdateSong($updateSongId: ID!, $trackName: String!, $artistName: String!, $genre: String!) {
+          updateSong(id: $updateSongId, trackName: $trackName, artistName: $artistName, genre: $genre)
+        }`;
+
+    const {data: {updateSong} } = await server.executeOperation({
+        query: UPDATE_SONG,
+        variables: {
+            updateSongId: "53209e68-b2c9-404b-8037-5c458be9fc7c",
+            trackName: "Mona Lisa",
+            artistName: "Lil Wayne ft Kendrick Lamar",
+            genre: "Hip Hop"
+        }
+    });
+    expect(updateSong).toEqual("Updated Successfully!");
+});
+
+test('Update song failed', async () => {
+    const UPDATE_SONG = gql`
+        mutation UpdateSong($updateSongId: ID!, $trackName: String!, $artistName: String!, $genre: String!) {
+          updateSong(id: $updateSongId, trackName: $trackName, artistName: $artistName, genre: $genre)
+        }`;
+
+    const {errors: [error] } = await server.executeOperation({
+        query: UPDATE_SONG,
+        variables: {
+            updateSongId: "ffgg09e68-bdff9-4fs4b-8037-5c458be9fddff",
+            trackName: "Mona Lisa",
+            artistName: "Lil Wayne ft Kendrick Lamar",
+            genre: "Hip Hop"
+        }
+    });
+    expect(error.message).toEqual("Updated Failed!");
+});
